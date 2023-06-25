78e9fb2c5da8495d225177e1438537312ef0c89c
thuannd <thuansmile2479@gmail.com>
2023-06-25 16:26:08 +0700

    b2: setting tailwind

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
- 
+import { useState } from "react";
 
 function App() {
+
+  const [word, setWord] = useState('')
+  console.log(word);
+
   return (
-    <div className="App">
-      app
+    <div className="flex gap-8 h-screen items-center justify-center">
+      <input type="text"
+        className="outline-none border border-blue-600 px-4 py-2 w-[450px]"
+        value={word}
+        onChange={e => setWord(e.target.value)}
+      />
+      <button type="button"
+        className="outline-none px-4 py-2 bg-blue-500 rounded-md text-white">
+        Add
+      </button>
     </div>
   );
 }
