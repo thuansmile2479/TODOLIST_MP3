40b55ef22d55fe76e91edec87e8151448b4cb6b4
thuannd <thuansmile2479@gmail.com>
2023-06-28 17:48:29 +0700

    b6: redux & router dom & nested router

--- src/App.js
+++ src/App.js
@@ -1,52 +1,20 @@
-import { useState } from "react";
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
+import { Home, Login, Public } from './containers/public'
+import { Routes, Route } from "react-router-dom";
+import path from './ultis/path';
 
-function App() {
 
-  const [word, setWord] = useState('')
-  const [todos, setTodos] = useState([])
-  const handleAdd = () => {
-    if (todos?.some(item => item.id === word?.replace(/\s+/g, ''))) {
-      toast.warn('Công việc đã đc thêm vào trc đó')
-    } else {
-      setTodos(prev => [...prev, { id: word?.replace(/\s+/g, ''), job: word }])
-      setWord('')
-    }
-
-  }
-  
-  const handleDelete = (id) => {
-    setTodos(prev => prev.filter(item => item.id !== id))
-  }
-
-  console.log(todos);
+function App() { 
   return (
-    <>
-      <div className="flex flex-col gap-8 h-screen items-center justify-center">
-        <div className="flex gap-8">
-          <input type="text"
-            className="outline-none border border-blue-600 px-4 py-2 w-[450px]"
-            value={word}
-            onChange={e => setWord(e.target.value)}
-          />
-          <button type="button"
-            className="outline-none px-4 py-2 bg-blue-500 rounded-md text-white"
-            onClick={handleAdd}>
-            Add
-          </button>
-        </div>
-        <h3 className="font-bold text-xl">Content:</h3>
-        <ul>
-          {todos?.map((item) => {
-            return (
-              <li key={item.id} className="flex gap-10 items-center">
-                <span>{item.job}</span>
-                <span onClick={() => handleDelete(item.id)} className="my-2 cursor-pointer p-2">X</span>
-              </li>
-            )
-          })}
-        </ul>
+    <> 
+      <div>
+        <Routes>
+          <Route path={path.PUBLIC} element={<Public />} >
+            <Route path={path.HOME} element={<Home />} />
+            <Route path={path.LOGIN} element={<Login />} />
+          </Route>
+        </Routes>
       </div>
       <ToastContainer
         position="bottom-right"
---
+++ src/store/reducers/appReducer.js
@@ -0,0 +1,19 @@
+import actionTypes from "../actions/actionTypes";
+
+const initState = {
+    homeData: [],
+    test: 'hello 123'
+}
+
+const appReducer = (state = initState, action) => {
+    switch (action.type) {
+        case actionTypes.GET_HOME:
+            return state
+            
+    
+        default:
+            return state
+    }
+}
+
+export default appReducer
