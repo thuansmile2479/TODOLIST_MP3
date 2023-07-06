c8b4ed651d7a227e37bcbad82695bc5d41f2f55d
thuannd <thuansmile2479@gmail.com>
2023-07-06 23:22:35 +0700

    B9: Set up APIs

--- src/containers/public/Home.js
+++ src/containers/public/Home.js
@@ -1,7 +1,19 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { Header } from '../../components'
+import * as apis from '../../apis'
 
 const Home = () => {
+
+  useEffect(() => {
+    const fetchDataHome = async () => {
+      const response = await apis.getHome()
+      console.log(response);
+    }
+
+    fetchDataHome()
+
+  }, [])
+
   return (
     <div className='overflow-y-auto'>
       <div className='h-[70px]  flex items-center'>
