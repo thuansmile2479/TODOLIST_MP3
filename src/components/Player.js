a99128df941ccecc58e2849db386d1b017336b2e
thuannd <thuansmile2479@gmail.com>
2023-07-19 02:29:16 +0700

    coding player 02

--- src/components/Player.js
+++ src/components/Player.js
@@ -3,23 +3,39 @@ import { useSelector } from 'react-redux'
 import * as apis from '../apis'
 import icons from '../ultis/icons'
 
-const { AiFillHeart, AiOutlineHeart, BsThreeDots } = icons
+const { AiOutlineHeart, AiFillHeart, BsThreeDots, MdSkipNext, MdSkipPrevious, CiRepeat, BsPauseFill, BsFillPlayFill, CiShuffle } = icons
 
 const Player = () => {
 
-    const { curSongId } = useSelector(state => state.music)
+    const audioEl = new Audio('https://mp3-s1-m-zmp3.zmdcdn.me/f46c9b2792677b392276/9094281577763410628?authen=exp=1667134635~acl=/f46c9b2792677b392276/*~hmac=972acda7d0ec6b777a75dc6c368eb130')
+    const { curSongId, isPlaying } = useSelector(state => state.music)
     const [songInfo, setSongInfo] = useState(null)
+    const [source, setSource] = useState(null)
+    console.log(audioEl)
 
     useEffect(() => {
         const fetchDetailSong = async () => {
-            const response = await apis.getDetailSong(curSongId)
-            // console.log(response);
-            if(response.data.err === 0){
-                setSongInfo(response.data.data)
+            const [res1, res2] = await Promise.all([
+                apis.getDetailSong(curSongId),
+                apis.getSong(curSongId)
+            ])
+            if (res1.data.err === 0) {
+                setSongInfo(res1.data.data)
             }
+            if (res2.data.err === 0) {
+                setSource(res2.data.data['128'])
+            }  
         }
         fetchDetailSong()
     }, [curSongId])
+
+    useEffect(() => {
+        audioEl.play()
+ 
+    }, [curSongId])
+
+    const handleTogglePlayMusic = () => {
+    }
 
     return (
         <div className='bg-main-400 px-5 h-full flex'>
@@ -38,8 +54,23 @@ const Player = () => {
                     </span>
                 </div>
             </div>
-            <div className='w-[40%] flex-auto border border-red-500'>
-                Main Player
+            <div className='w-[40%] flex-auto border flex items-center justify-center gap-2 flex-col border-red-500 py-2'>
+                <div className='flex gap-8 justify-center items-center'>
+                    <span className='cursor-pointer' title='Bật phát ngẫu nhiên'><CiShuffle size={24} /></span>
+                    <span className='cursor-pointer'><MdSkipPrevious size={24} /></span>
+                    <span
+                        className='p-1 border border-gray-700 cursor-pointer hover:text-main-500 rounded-full'
+                        onClick={handleTogglePlayMusic}
+                    >
+                        {isPlaying ? <BsPauseFill size={30} /> : <BsFillPlayFill size={30} />}
+                    </span>
+                    <span className='cursor-pointer'><MdSkipNext size={24} /></span>
+                    <span className='cursor-pointer' title='Bật phát lại tất cả'><CiRepeat size={24} /></span>
+
+                </div>
+                <div>
+                    progress bar
+                </div>
             </div>
             <div className='w-[30%] flex-auto border border-red-500'>
                 Volume
--- src/ultis/icons.js
+++ src/ultis/icons.js
@@ -1,10 +1,12 @@
-import { MdOutlineLibraryMusic, MdOutlineFeed } from 'react-icons/md' 
+import { MdOutlineLibraryMusic, MdOutlineFeed, MdSkipPrevious,
+    MdSkipNext} from 'react-icons/md' 
 import { FiPieChart } from 'react-icons/fi' 
 import { TbChartArcs } from 'react-icons/tb' 
 import { HiArrowNarrowLeft, HiArrowNarrowRight } from 'react-icons/hi' 
 import { FiSearch } from 'react-icons/fi' 
-import { AiFillHeart, AiOutlineHeart } from 'react-icons/ai' 
-import { BsThreeDots } from 'react-icons/bs' 
+import { AiFillHeart, AiOutlineHeart } from 'react-icons/ai'  
+import { CiRepeat, CiShuffle } from 'react-icons/ci'
+import { BsThreeDots, BsFillPlayFill, BsPauseFill, BsMusicNoteBeamed, BsDot } from 'react-icons/bs'
 
 
 const icons = {
@@ -17,7 +19,15 @@ const icons = {
     FiSearch,
     AiFillHeart,
     AiOutlineHeart,
-    BsThreeDots
+    BsThreeDots,
+    CiRepeat,
+    MdSkipPrevious,
+    MdSkipNext,
+    CiShuffle,
+    BsFillPlayFill,
+    BsPauseFill,
+    BsMusicNoteBeamed,
+    BsDot
 }
 
 export default icons
